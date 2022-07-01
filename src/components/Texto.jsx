59559f6ab714ebe0f8a5dacf8c0e9aab7d50fe00
CarlosJunioor <carlosjunior866@gmail.com>
2022-07-01 04:47:18 +0100

    react001

---
+++ src/components/Texto.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Texto () {
+    return (
+        <div>
+            <h2>Texto</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sint illo nesciunt vero, quisquam veritatis dolor libero quod eveniet velit, commodi earum culpa ipsam eaque reiciendis totam obcaecati, quam amet?</p>
+        </div>
+
+    )
+}
+export default Texto
